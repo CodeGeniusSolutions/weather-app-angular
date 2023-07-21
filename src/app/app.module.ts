220d59b655a5c9388597601da0685bcaab46e7dd
AndreiDetenkov <32195682+AndreiDetenkov@users.noreply.github.com>
2023-07-21 16:55:23 +0600

    feat: add weather module with component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { WeatherModule } from './weather/infrastructure/weather.module'
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, WeatherModule],
   providers: [],
   bootstrap: [AppComponent],
 })
